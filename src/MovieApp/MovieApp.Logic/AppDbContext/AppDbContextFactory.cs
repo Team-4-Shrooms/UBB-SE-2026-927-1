db18c3c5b4217d354126ed707464cbffa0d7dca3
TaschinaLuca <taschina529@gmail.com>
2026-04-25 20:15:07 +0300

    Some installs later

---
+++ src/MovieApp/MovieApp.Logic/AppDbContext/AppDbContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApp.Logic.Data
+{
+    // This interface tells EF Core tools: "Use this class to build the context when running terminal commands!"
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+
+            // Tell it we are using SQL Server. 
+            // Note: This connection string is ONLY used to generate the migration files. 
+            // It does not matter if this specific database actually exists on your machine right now.
+            optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=MovieApp;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=True;");
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
